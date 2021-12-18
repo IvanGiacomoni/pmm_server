8817815d8c5ca27abd97a54ef7b0810870c49033
dani3b <danielebufalieritkd@gmail.com>
2021-12-18 16:36:21 +0100

    rdigu

--- routes/utility.js
+++ routes/utility.js
@@ -2,15 +2,23 @@ const express = require('express')
 const router = express.Router()
 const config = require('config')
 
+const Prodotti=['Bracciale','USB','Powerbank','Scatola']
+const Quantity=[3,5,0,4]
 
+i=0
 router.get('/', async (req,res) => {  
-    res.status(200).send("<?xml version='1.0'?><Product_List>  <ProductCost>5</ProductCost>    <ProductId>'1'</ProductId>    <ProductName>'Bracciale'</ProductName>    <Quantity>4</Quantity>  </Product_List>")
+
+    if(i>=Prodotti.length)
+        i=0
+    var prodotto=Prodotti[i]
+    var quant=Quantity[i]
+    var costo=Math.random()%10
+    res.status(200).send("<?xml version='1.0'?><Product_List>  <ProductCost>"+costo+"</ProductCost>    <ProductId>'"+i+"'</ProductId>    <ProductName>'"+prodotto+"'</ProductName>    <Quantity>"+quant+"</Quantity>  </Product_List>")
+    i=i+1
 })
 
 
 
-router.get('/2', async (req,res) => {  
-    res.status(200).send("<?xml version='1.0'?><Product_List>  <ProductCost>10</ProductCost>    <ProductId>'2'</ProductId>    <ProductName>'Bracciale'</ProductName>    <Quantity>4</Quantity>  </Product_List>")
-})
+
 
 module.exports = router
