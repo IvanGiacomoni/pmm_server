c4a073e4356f0c455429a3708cc2937ea75b7e6f
dani3b <danielebufalieritkd@gmail.com>
2021-12-20 09:11:28 +0100

    yeye

--- routes/utility.js
+++ routes/utility.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const config = require('config')
+const nodemailer = require("nodemailer");
 
 const Prodotti=['Bracciale','USB','Powerbank','Scatola']
 const Quantity=[3,5,0,4]
@@ -12,17 +13,82 @@ router.get('/', async (req,res) => {
         i=0
     var prodotto=Prodotti[i]
     var quant=Quantity[i]
-    var costo=Math.random()%10
+    var costo= Math.floor(Math.random() * 10);
+
     res.status(200).send("<?xml version='1.0'?><Product_List>  <ProductCost>"+costo+"</ProductCost>    <ProductId>'"+i+"'</ProductId>    <ProductName>'"+prodotto+"'</ProductName>    <Quantity>"+quant+"</Quantity>  </Product_List>")
     i=i+1
 })
 
 
 
-router.post('/sendEmail',async (req,res)=>{
-    console.log(req)
+router.post('/sendEmail/:CustomerEmail/:CustomerName/:CustomerSurname/:message',async (req,res)=>{
+    var email=req.params.CustomerEmail
+    var name=req.params.CustomerName
+    var surname=req.params.CustomerSurname
+    var message=req.params.message
+
+    let transporter = nodemailer.createTransport({
+        host: "smtp.libero.it",
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+          user: "progetto_pmm@libero.it", // generated ethereal user
+          pass: "Progettopmm2022.", // generated ethereal password
+        },
+      });
+      
+      let info = await transporter.sendMail({
+        from: "progetto_pmm@libero.it", // sender address
+        to: email, // list of receivers
+        subject: "Order Info", // Subject line
+        text: "Dear "+ name+" "+ surname+", \n"+message , // plain text body
+
+      });
+    
+    
     res.status(200).send("<?xml version='1.0'?><done>'Fatto'</done>")
 })
+
+
+
+
+router.get('/sendPrototype/:CustomerEmail/:CustomerName/:CustomerSurname/:customization',async (req,res)=>{
+    var email=req.params.CustomerEmail
+    var name=req.params.CustomerName
+    var surname=req.params.CustomerSurname
+    var customization=req.params.customization
+
+
+    let transporter = nodemailer.createTransport({
+        host: "smtp.libero.it",
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+          user: "progetto_pmm@libero.it", // generated ethereal user
+          pass: "Progettopmm2022.", // generated ethereal password
+        },
+      });
+      
+      let info = await transporter.sendMail({
+        from: "progetto_pmm@libero.it", // sender address
+        to: email, // list of receivers
+        subject: "Order Info", // Subject line
+        text: "Dear "+ name+" "+ surname+", \n"+"this is your prototype: "+customization , // plain text body
+
+      });
+
+      var resp=Math.floor(Math.random() * 2);
+
+      console.log(resp)
+      if(resp==0)
+        resp=false
+      else
+        resp=true
+
+    
+    res.status(200).send("<?xml version='1.0'?><response>"+resp+"</response>")
+})
+
 
 
 
